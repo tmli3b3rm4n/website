f237222f9ca351fc80e9d1ef0127ffa69c746e8c
tmli3b3rm4n <tim_lieberman@yahoo.com>
2017-08-29 02:15:03 -0400

    getting users installed

---
+++ tests/TestingBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TestingBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
