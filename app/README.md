500be0818c47e3258964bed07e3a1631000009df
tmli3b3rm4n <tim_lieberman@yahoo.com>
2017-09-04 03:11:40 -0400

    Changes for docker

--- app/README.md
+++
@@ -1,4 +0,0 @@
-app
-===
-
-A Symfony project created on August 21, 2017, 3:14 pm.
--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -3,13 +3,9 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
